1d16ee52ab9d8e8a4f3fdd16f2e3308a3c773224
Mati K <matijklocek@gmail.com>
2025-05-31 12:07:42 +0100

    2.2

---
+++ 0/1-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "What is your name? ";
+    std::string name;
+    
+
+    return 0;
+}
--- 0/small_prog.cpp
+++ 0/small_prog.cpp
@@ -1,8 +1,26 @@
-// A small c++ program
+// Ask for person's name, and greet the person
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Please enter your first name: ";
+    std::string name;
+    std::cin >> name;
+    
+    const std::string exclam = "!";
+    const std::string greeting = "Hello, " + name + exclam;
+
+    const std::string spaces(greeting.size(), ' ');
+    const std::string second = "* " + spaces + " *";
+    const std::string first(second.size(), '*');
+    
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+
     return 0;
 }
